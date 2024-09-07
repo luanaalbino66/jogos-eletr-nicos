d4cea9d46f51d75d4b8ce2133959a78a9b84f109
luanaalbino66 <luanaalbino6@hotmail.com>
2024-09-07 00:07:56 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,44 @@
+let dados = [
+    {
+      titulo: "Grand Theft Auto",
+      descricao: "O Grand Theft Auto é uma das franquias de videogames mais populares e controversas do mundo. Conhecida por seus mundos abertos vastos e detalhados, a série GTA permite que os jogadores explorem cidades fictícias, realizem missões e vivam uma vida virtual repleta de liberdade e caos. A maior parte da jogabilidade gira em torno de dirigir e atirar, com ocasionais elementos furtivos e role-playing",
+      link: "https://pt.wikipedia.org/wiki/Grand_Theft_Auto",
+      tags: "jogos missões violência violencia mundo aberto carros simulaçao simulação"
+    },
+    {
+      titulo: "FIFA (EA Sports FC)",
+      descricao: "FIFA, agora EA Sports FC, é uma das franquias de jogos de futebol mais populares do mundo. Simula a experiência de jogar futebol profissional, permitindo aos jogadores controlar seus times favoritos, criar seus próprios times e competir em diversos modos de jogo. A série é conhecida por suas licenças oficiais de ligas, equipes e jogadores.",
+      link: "https://pt.wikipedia.org/wiki/EA_Sports_FC_(s%C3%A9rie)",
+      tags: "jogo futebol times jogadores"
+    },
+    {
+      titulo: "Need for Speed",
+      descricao: "Need for Speed é uma franquia de jogos de corrida que coloca os jogadores no volante de carros esportivos de alta performance. A série oferece uma variedade de modos de jogo, desde corridas de rua ilegais até campeonatos profissionais. A franquia é conhecida por sua jogabilidade emocionante e gráficos realistas.",
+      link: "https://pt.wikipedia.org/wiki/Need_for_Speed",
+      tags: "jogo carro corrida velocidade "
+    },
+    {
+      titulo: "Assassin's Creed",
+      descricao: "Assassin's Creed é uma série de jogos de ação e aventura em mundo aberto que coloca os jogadores no papel de assassinos históricos. A série explora temas como conspiração, política e religião, enquanto os jogadores se infiltram em cidades e fortalezas para completar suas missões. A franquia é conhecida por sua rica história e jogabilidade envolvente.",
+      link: "https://pt.wikipedia.org/wiki/Assassin's_Creed",
+      tags: "jogo espada luta violência violencia"
+    },
+    {
+      titulo: "The Sims",
+      descricao: "The Sims é uma série de jogos de simulação de vida que permite aos jogadores criar e controlar seus próprios personagens virtuais. Os jogadores podem construir casas, criar relacionamentos, seguir carreiras e experimentar diferentes estilos de vida. A série é conhecida por sua jogabilidade aberta e personalização.",
+      link: "https://pt.wikipedia.org/wiki/The_Sims",
+      tags: "jogo simulaçao simulação vida real casas sims"
+    },
+    {
+      titulo: "Call of Duty",
+      descricao: "Call of Duty é uma das franquias de jogos de tiro em primeira pessoa mais populares do mundo. A série oferece uma variedade de experiências, desde campanhas single-player baseadas em eventos históricos até modos multiplayer competitivos. A franquia é conhecida por seus gráficos realistas, jogabilidade intensa e som imersivo.",
+      link: "https://pt.wikipedia.org/wiki/Call_of_Duty",
+      tags: "jogo tiro missões missão violência violencia"
+    },
+    {
+      titulo: "Watch Dogs",
+      descricao: "Watch Dogs é uma série de jogos de mundo aberto que se passa em um futuro próximo onde a tecnologia controla todos os aspectos da vida. Os jogadores assumem o papel de hackers que podem invadir sistemas e manipular o ambiente ao seu redor. A franquia é conhecida por sua história intrigante e jogabilidade inovadora.",
+      link: "https://pt.wikipedia.org/wiki/Watch_Dogs",
+      tags: "jogo tecnologia hackers mundo aberto"
+    }
+  ];
